1fc95eb43a1986276367aa5755a5201b36a32d88
Vinay-Krishna-G <vinaykrishna1515@gmail.com>
2026-07-01 10:31:40 +0530

    refactor: replace ConfiguratorV2 with ComposerV2 component on the home page

--- apps/website/src/app/page.tsx
+++ apps/website/src/app/page.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import { HeroV2 } from '../components/home/v2/HeroV2';
 import { WorldScrollV2 } from '../components/home/v2/WorldScrollV2';
 import { ValuePropV2 } from '../components/home/v2/ValuePropV2';
-import { ConfiguratorV2 } from '../components/home/v2/ConfiguratorV2';
+import { ComposerV2 } from '../components/home/v2/ComposerV2';
 
 export default function Home() {
   return (
     <main style={{ background: '#09090B', color: '#FAFAFA' }}>
       <HeroV2 />
-      <ConfiguratorV2 />
+      <ComposerV2 />
       <WorldScrollV2 />
       <ValuePropV2 />
       
